00565defe923a0be1cf3c43f880a202d2327f9cf
Fergal Moran <fergal.moran@gmail.com>
2015-06-12 22:21:12 +0100

    Added optional slash to social urls

--- spa/social/urls.py
+++ spa/social/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^playlist/(?P<slug>[\w\d_.-]+)/$', 'spa.social.views.playlist', name='social_playlist_slug'),
-    url(r'^mix/(?P<slug>[\w\d_.-]+)/$', 'spa.social.views.facebook_mix', name='social_mix_slug'),
-    url(r'^user/(?P<user_id>\w+)/$', 'spa.social.views.user', name='social_user'),
-    url(r'^like/(?P<mix_id>\d+)/$', 'spa.social.views.post_like', name='social_like'),
+    url(r'^playlist/(?P<slug>[\w\d_.-]+)/?$', 'spa.social.views.playlist', name='social_playlist_slug'),
+    url(r'^mix/(?P<slug>[\w\d_.-]+)/?$', 'spa.social.views.facebook_mix', name='social_mix_slug'),
+    url(r'^user/(?P<user_id>\w+)/?$', 'spa.social.views.user', name='social_user'),
+    url(r'^like/(?P<mix_id>\d+)/?$', 'spa.social.views.post_like', name='social_like'),
     url(r'^$', 'spa.social.views.index', name='social_index')
 )
