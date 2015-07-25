59cb70b120aac36fb53afe9c42088100e3b22db9
Fergal Moran <fergal.moran@gmail.com>
2015-07-26 00:34:18 +0100

    Added var for redis host

--- core/realtime/chat.py
+++ core/realtime/chat.py
@@ -1,10 +1,10 @@
 import json
 import datetime
 import redis
-
+import settings
 
 def post_chat(session, image, user, message):
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r = redis.StrictRedis(host=settings.REDIS_HOST, port=6379, db=0)
     payload = json.dumps({
         'session': session,
         'message': message,
