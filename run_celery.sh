364ad61431f23d917cc7eadcf72176770b191ad0
Fergal Moran <fergal.moran@gmail.com>
2015-07-28 22:04:38 +0100

    Updated tasks

--- run_celery.sh
+++ run_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-su -m djworker -c "celery worker -A dss.celeryconf -Q default"
+su -m djworker -c "sleep 3 && celery worker -A dss.celeryconf -Q default"
 chown djworker /files -R
 chown djworker /tmp/dss.log
