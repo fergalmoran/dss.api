4a98536ddbe059c2e1917be8edc61d2f85854aa5
Fergal Moran <fergal.moran@gmail.com>
2015-11-11 00:10:33 +0000

    Embed kinda done

--- spa/embedding/views.py
+++ spa/embedding/views.py
@@ -20,6 +20,7 @@ def mix(request, **args):
     payload = {
         "description": mix.description.replace('<br />', '\n'),
         "title": mix.title,
+        "theme": "light",
         "image_url": image,
         "audio_url": audio_url,
         "mix_url": 'http://%s%s' % (Site.objects.get_current().domain, mix_url)
