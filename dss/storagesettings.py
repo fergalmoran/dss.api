e844ca6ed4c28b4d38f07887df51e9cd68e026ba
Fergal Moran <fergal.moran@gmail.com>
2015-11-16 19:24:46 +0000

    Added variable for CDN name

--- dss/storagesettings.py
+++ dss/storagesettings.py
@@ -1,6 +1,9 @@
 from dss import localsettings
+import os
 
-AZURE_ACCOUNT_NAME = 'dsscdn'
+print("Importing storage settings")
+
+AZURE_ACCOUNT_NAME = os.environ.get('CDN_NAME', 'dsscdn')
 AZURE_CONTAINER = 'media'
 AZURE_ACCOUNT_KEY = localsettings.AZURE_ACCOUNT_KEY
-AZURE_ITEM_BASE_URL = 'https://dsscdn.blob.core.windows.net/'
+AZURE_ITEM_BASE_URL = 'https://{}.blob.core.windows.net/'.format(AZURE_ACCOUNT_NAME)
